7e24b6ad1a71eefa0ad176907c9026b6faf37a4e
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-10-18 16:28:55 -0300

    #animation 3 Growing Circle

--- lib/implicit_examples/animated_tween_animation_builder_example.dart
+++ lib/implicit_examples/animated_tween_animation_builder_example.dart
@@ -1,31 +1,59 @@
 import 'package:flutter/material.dart';
 
-class PulsatingCircleAnimation extends StatelessWidget {
+class PulsatingCircleAnimation extends StatefulWidget {
   const PulsatingCircleAnimation({super.key});
-  final double size = 200;
 
+  @override
+  State<PulsatingCircleAnimation> createState() =>
+      _PulsatingCircleAnimationState();
+}
+
+class _PulsatingCircleAnimationState extends State<PulsatingCircleAnimation> {
+  int i = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pulsating Circle Animation'),
+        title: Text('Pulsating Circle $i'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                i = ++i;
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              elevation: 0,
+            ),
+            child: const Text('Refresh'),
+          ),
+        ],
       ),
       body: Center(
-        child: Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
-              ),
-            ],
-          ),
-        ),
+        child: TweenAnimationBuilder(
+            key: ValueKey(i),
+            curve: Curves.easeOutExpo,
+            tween: Tween<double>(begin: 0, end: 200),
+            duration: const Duration(milliseconds: 1200),
+            builder: (context, object, widget) {
+              return Container(
+                width: object,
+                height: object,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.5),
+                      blurRadius: object,
+                      spreadRadius: object / 2,
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
